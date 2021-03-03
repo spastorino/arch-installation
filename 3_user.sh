88c091cc7cc7502d719e4fad5e4b50aa1318cf91
Santiago Pastorino <spastorino@gmail.com>
2021-03-03 00:05:24 -0300

    Update Rust installation

--- 3_user.sh
+++ 3_user.sh
@@ -3,15 +3,15 @@
 # Install Rust stable, beta and nightly versions
 rustup install stable
 rustup default stable
-rustup component add clippy llvm-tools-preview rls rust-analysis rust-src rustc-dev rustfmt
+rustup component add llvm-tools-preview rls rust-analysis rust-src rustc-dev rustc-docs
 
 rustup install beta
 rustup default beta
-rustup component add clippy llvm-tools-preview rls rust-analysis rust-src rustc-dev rustfmt
+rustup component add llvm-tools-preview rls rust-analysis rust-src rustc-dev rustc-docs
 
 rustup install nightly
 rustup default nightly
-rustup component add clippy llvm-tools-preview miri rls rust-analysis rust-analyzer-preview rustc-dev rust-src rustfmt
+rustup component add llvm-tools-preview miri rls rust-analysis rust-analyzer-preview rust-src rustc-dev rustc-docs
 rustup target install i686-unknown-linux-gnu
 
 # Install cargo packages
@@ -23,21 +23,21 @@ cargo install cargo-bloat
 cargo install cargo-binutils
 cargo install cargo-check
 cargo install cargo-clone
-cargo install cargo-count
+# cargo install cargo-count # clap didn't work
 cargo install cargo-crev
 cargo install cargo-deadlinks
 cargo install cargo-edit
 cargo install cargo-expand
-cargo install cargo-graph
-# cargo install cargo-info # didn't work
+# cargo install cargo-graph # clap didn't work
+# cargo install cargo-info # openssl didn't work
 cargo install cargo-license
 cargo install cargo-make
-# cargo install cargo-modules # didn't work
+# cargo install cargo-modules # a feature didn't work
 cargo install cargo-open
 cargo install cargo-outdated
-cargo install cargo-profiler
+# cargo install cargo-profiler # rustc_version didn't work
 cargo install cargo-release
-cargo install cargo-script
+# cargo install cargo-script # rustc_version didn't work
 cargo install cargo-sweep
 cargo install cargo-tarpaulin
 cargo install cargo-tree
