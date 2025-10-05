6e663f2200f6e00e3295ab41dc622aa8a61880e9
Santiago Pastorino <spastorino@gmail.com>
2025-10-05 18:14:51 -0300

    Add keychron udev rules and firmware upgrade documentation

--- 3_setup_custom.sh
+++ 3_setup_custom.sh
@@ -5,6 +5,14 @@ cat <<EOT > /etc/udev/rules.d/99-logitech-webcam.rules
 SUBSYSTEM=="video4linux", KERNEL=="video[0-9]*", ATTRS{product}=="Logi Webcam C920e", ATTR{index}=="0", RUN+="/usr/bin/v4l2-ctl -c focus_automatic_continuous=0"
 EOT
 
+# Keychron keyboard, to upgrade most of the bios (mouse and wireless keyboard),
+# visit https://launcher.keychron.com/ and may be useful also https://usevia.app/
+# To upgrade keychron keyboard firmware the only way seems to be:
+# dfu-util -a 0 --dfuse-address 0x08000000:leave -D k1_max_ansi_white_v1.1.1_2504231209.bin
+cat <<EOT > /etc/udev/rules.d/99-via.rules
+KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0666", TAG+="uaccess", TAG+="udev-acl"
+EOT
+
 echo "Namecheap dynamic dns password? Advanced DNS -> Dynamic DNS Password"
 read -s dyndns_pass
 echo
