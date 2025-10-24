f2dc0c58fe4d2fd8d6c090d0b72e938ad235aa58
Santiago Pastorino <spastorino@gmail.com>
2025-10-24 16:12:57 -0300

    Remove and update some aur packages

--- 8_aur.sh
+++ 8_aur.sh
@@ -3,12 +3,6 @@
 mkdir -p ~/src/aur
 pushd ~/src/aur
 
-git clone https://aur.archlinux.org/beersmith.git
-pushd beersmith
-makepkg -sir
-git clean -xdf
-popd
-
 git clone https://aur.archlinux.org/brother-hll2350dw.git
 pushd brother-hll2350dw
 makepkg -sir
@@ -27,56 +21,8 @@ popd
 
 lpadmin -p DYMO -D "DYMO LabelWriter 450 Turbo" -E -v "usb://DYMO/LabelWriter%20450%20Turbo?serial=16071714230436" -m lw450t.ppd
 
-git clone https://aur.archlinux.org/emsdk.git
-pushd emsdk
-makepkg -sir
-git clean -xdf
-popd
-
-git clone https://aur.archlinux.org/flamegraph.git
-pushd flamegraph
-makepkg -sir
-git clean -xdf
-popd
-
-git clone https://aur.archlinux.org/git-imerge-git.git
-pushd git-imerge-git
-makepkg -sir
-git clean -xdf
-popd
-
-git clone https://aur.archlinux.org/gnome-shell-extension-system76-power-git.git
-pushd gnome-shell-extension-system76-power-git
-makepkg -sir
-git clean -xdf
-popd
-
-# Dependencies of hotspot
-git clone https://aur.archlinux.org/rustc-demangle.git
-pushd rustc-demangle
-makepkg -sir
-git clean -xdf
-popd
-git clone https://aur.archlinux.org/kddockwidgets.git
-pushd kddockwidgets
-makepkg -sir
-git clean -xdf
-popd
-
-git clone https://aur.archlinux.org/hotspot.git
-pushd hotspot
-makepkg -sir
-git clean -xdf
-popd
-
-git clone https://aur.archlinux.org/servo-latest.git
-pushd servo-latest
-makepkg -sir
-git clean -xdf
-popd
-
-git clone https://aur.archlinux.org/spotify.git
-pushd spotify
+git clone https://aur.archlinux.org/gnome-shell-extension-system76-power.git
+pushd gnome-shell-extension-system76-power
 makepkg -sir
 git clean -xdf
 popd
@@ -87,26 +33,20 @@ makepkg -sir
 git clean -xdf
 popd
 
-git clone https://aur.archlinux.org/teams.git
-pushd teams
-makepkg -sir
-git clean -xdf
-popd
-
 git clone https://aur.archlinux.org/virtualbox-ext-oracle.git
 pushd virtualbox-ext-oracle
 makepkg -sir
 git clean -xdf
 popd
 
-git clone https://aur.archlinux.org/zoom.git
-pushd zoom
+git clone https://aur.archlinux.org/system76-dkms-git.git
+pushd system76-dkms-git
 makepkg -sir
 git clean -xdf
 popd
 
-git clone https://aur.archlinux.org/system76-dkms-git.git
-pushd system76-dkms-git
+git clone https://aur.archlinux.org/firmware-manager.git
+pushd firmware-manager
 makepkg -sir
 git clean -xdf
 popd
